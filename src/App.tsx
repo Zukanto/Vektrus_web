613177623c102dc6b3da05a76f283e3ecaf20af4
Zukanto <jan.breidenbach@mediaolymp.de>
2024-12-06 13:23:05 +0100

    Initialer Commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Features from './components/Features';
+import DataEngine from './components/DataEngine';
+import RoiCalculator from './components/RoiCalculator';
+import OptimizationPreview from './components/OptimizationPreview';
+import CaseStudy from './components/CaseStudy';
+import FeaturesExplorer from './components/FeaturesExplorer';
+import SuccessStories from './components/SuccessStories';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900">
+      <Navbar />
+      <Hero />
+      <Features />
+      <DataEngine />
+      <RoiCalculator />
+      <OptimizationPreview />
+      <CaseStudy />
+      <FeaturesExplorer />
+      <SuccessStories />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
